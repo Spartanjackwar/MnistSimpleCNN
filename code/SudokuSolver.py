104b9d1a418e5af909beeedd7bc83e85291386e2
Spartanjackwar <jackwmoss@gmail.com>
2024-07-01 08:54:14 -0700

    Fixed run errors related to none type contour returns.  Note that this simply means that we have avoided the error, not solved it as the polygons in the frame are not known to be properly found right now.

--- code/SudokuSolver.py
+++ code/SudokuSolver.py
@@ -6,9 +6,19 @@ import imutils
 from matplotlib import pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
+from models.modelM5 import ModelM5
+import torch
+
+# enable GPU usage ------------------------------------------------------------#
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+if use_cuda == False:
+    print("WARNING: CPU will be used.")
+    exit(0)
 
 #Load the model
-new_model = tf.keras.models.load_model('Digit_Recognizer.h5')
+#new_model = tf.keras.models.load_model('Digit_Recognizer.h5')
+new_model = ModelM5().to(device)
 
 #Parameters for Warping the image
 margin = 10
@@ -61,6 +71,7 @@ def solveSudoku(grid, i=0, j=0):
             grid[i][j] = 0
     return False
 
+
 '''Capture the contour from the capture'''
 #Gets predicted digit nested list
 def display_predList(predList):
@@ -70,13 +81,15 @@ def display_predList(predList):
             predicted_digits.append(predList[j][i])
     return predicted_digits
 
+
 def processFrameForContour(frame):
     #Process the frame to find contour
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (5, 5), 0)
     thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 9, 2)
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    return contours
+    contours_, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    return contours_
+
 
 def FindLargestContour(contours):
     # Find the largest contour(Sudoku Grid)
@@ -96,7 +109,7 @@ def FindLargestContour(contours):
 
 
 # Draw the contour and extract Sudoku Grid
-def ExtractGrid(contour):
+def ExtractGrid(contour, frame):
     if contour is None:
         return
 
@@ -140,86 +153,86 @@ def ExtractGrid(contour):
     invert_window = invert_window / 255
     i = 0
 
-    return result, invert, invert_window, matrix
+    return result, invert, invert_window, matrix, p_window
 
 while True:
-    ret, frame=cap.read()
+    ret, frame = cap.read()
     p_frame = frame.copy()
 
     contours = processFrameForContour(frame)
     largestContour = FindLargestContour(contours)
     contour = largestContour[0]
     maxArea = largestContour[1]
+    if contour is not None:
+        result, invert, invert_window, matrix, p_window = ExtractGrid(largestContour, frame)
 
-    result, invert, invert_window, matrix = ExtractGrid(contours)
-
-    # Check if the answer has been already predicted or not
-    # If not predict the answer
-    # Else only get the cell regions
-    if flag != 1:
-        predicted_digits = []
-        pixels_sum = []
-
-    # To get individual cells
-    for y in range(9):
-        predicted_line = []
-        for x in range(9):
-            y2min = y * case + margin
-            y2max = (y + 1) * case - margin
-            x2min = x * case + margin
-            x2max = (x + 1) * case - margin
-
-            # Obtained Cell
-            image = invert_window[y2min:y2max, x2min:x2max]
-
-            # Process the cell to feed it into model
-            img = cv2.resize(image, (28, 28))
-            img = img.reshape((1, 28, 28, 1))
-
-            # Get sum of all the pixels in the cell
-            # If sum value is large it means the cell is blank
-            pixel_sum = np.sum(img)
-            pixels_sum.append(pixel_sum)
-
-            # Predict the digit in the cell
-            pred = new_model.predict(img)
-            predicted_digit = pred.argmax()
-
-            # For blank cells set predicted digit to 0
-            if pixel_sum > 775.0:
-                predicted_digit = 0
-
-            predicted_line.append(predicted_digit)
-
-            # If we already have predicted result, display it on window
-            if flag == 1:
-                ans = 1
-                x_pos = int((x2min + x2max) / 2) + 10
-                y_pos = int((y2min + y2max) / 2) - 5
-                image = cv2.putText(result, str(pred_digits[i]), (y_pos, x_pos), cv2.FONT_HERSHEY_SIMPLEX,
-                                    1, (255, 0, 0), 2, cv2.LINE_AA)
-
-            i = i + 1
-
-        # Get predicted digit list
+        # Check if the answer has been already predicted or not
+        # If not predict the answer
+        # Else only get the cell regions
         if flag != 1:
-            predicted_digits.append(predicted_line)
+            predicted_digits = []
+            pixels_sum = []
 
-        # Get solved Sudoku
-        ans = solveSudoku(predicted_digits)
-        if ans == True:
-            flag = 1
-            pred_digits = display_predList(predicted_digits)
+        # To get individual cells
+        for y in range(9):
+            predicted_line = []
+            for x in range(9):
+                y2min = y * case + margin
+                y2max = (y + 1) * case - margin
+                x2min = x * case + margin
+                x2max = (x + 1) * case - margin
 
-            # Display the final result
-            if ans == 1:
-                cv2.imshow("Result", result)
-                frame = cv2.warpPerspective(result, matrix, (perspective_size, perspective_size),
-                                            flags=cv2.WARP_INVERSE_MAP)
+                # Obtained Cell
+                image = invert_window[y2min:y2max, x2min:x2max]
 
-        cv2.imshow("frame", frame)
-        cv2.imshow('P-Window', p_window)
-        cv2.imshow('Invert', invert)
+                # Process the cell to feed it into model
+                img = cv2.resize(image, (28, 28))
+                img = img.reshape((1, 28, 28, 1))
+
+                # Get sum of all the pixels in the cell
+                # If sum value is large it means the cell is blank
+                pixel_sum = np.sum(img)
+                pixels_sum.append(pixel_sum)
+
+                # Predict the digit in the cell
+                pred = new_model.predict(img)
+                predicted_digit = pred.argmax()
+
+                # For blank cells set predicted digit to 0
+                if pixel_sum > 775.0:
+                    predicted_digit = 0
+
+                predicted_line.append(predicted_digit)
+
+                # If we already have predicted result, display it on window
+                if flag == 1:
+                    ans = 1
+                    x_pos = int((x2min + x2max) / 2) + 10
+                    y_pos = int((y2min + y2max) / 2) - 5
+                    image = cv2.putText(result, str(pred_digits[i]), (y_pos, x_pos), cv2.FONT_HERSHEY_SIMPLEX,
+                                        1, (255, 0, 0), 2, cv2.LINE_AA)
+
+                i = i + 1
+
+            # Get predicted digit list
+            if flag != 1:
+                predicted_digits.append(predicted_line)
+
+            # Get solved Sudoku
+            ans = solveSudoku(predicted_digits)
+            if ans == True:
+                flag = 1
+                pred_digits = display_predList(predicted_digits)
+
+                # Display the final result
+                if ans == 1:
+                    cv2.imshow("Result", result)
+                    frame = cv2.warpPerspective(result, matrix, (perspective_size, perspective_size),
+                                                flags=cv2.WARP_INVERSE_MAP)
+
+            cv2.imshow("frame", frame)
+            cv2.imshow('P-Window', p_window)
+            cv2.imshow('Invert', invert)
 
     cv2.imshow("frame", frame)
 
